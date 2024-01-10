f9f96ae7ebcac7ea66960bb00251949cf08c2251
MathieuGTR <mathieu@gtrsuite.com>
2024-01-10 17:14:04 +0100

    feat(frontend): display JSON as list + add loader

--- src/components/CountryListFetch.vue
+++ src/components/CountryListFetch.vue
@@ -5,7 +5,8 @@ const componentName = 'CountryListFetch';
 const apiToken = import.meta.env.VITE_GEONAMES_TOKEN;
 const apiURL = import.meta.env.VITE_GEONAMES_URL;
 const locale = ref('');
-let responseContent = ref('');
+const responseContent = ref('');
+const loader = ref(false);
 const options = ref([
     { text: 'FR', value: 'fr' },
     { text: 'EN', value: 'en' },
@@ -14,6 +15,7 @@ const options = ref([
 
 async function loadCountryListByLocale() {
     try {
+        loader.value = true;
         const response = await fetch(
             apiURL + '/country/list/' + locale.value,
             {
@@ -22,22 +24,22 @@ async function loadCountryListByLocale() {
             }
         );
         responseContent.value = await response.json();
-        console.log(responseContent);
     } catch (error) {
         responseContent = 'Error! Could not reach the API : ' + error;
         console.error(error);
     }
+    loader.value = false;
 };
 
 onUpdated(() => {
-    console.log('TODO : update content !');
+    //console.log('TODO : update content !');
 })
 
 </script>
 
 <template>
     <div>
-        <p>Get the country list in : {{ locale }}</p>
+        <p>Display the country list in : {{ locale }}</p>
         <select v-model="locale">
             <option disabled value="">Please select a locale</option>
             <option v-for="option in options" :value="option.value">
@@ -46,7 +48,53 @@ onUpdated(() => {
         </select>
         <button @click="loadCountryListByLocale">Display</button>
     </div>
+
+    <span class="loader" v-if="loader"></span>
     <div class="responsecontent" v-if="responseContent">
-        <p id="responsetext">Response : <br /> {{ responseContent }}</p>
+        <ul v-for="country in responseContent">
+            <li>CountryCode : <b>{{ country.countryCode }}</b></li>
+            <li>GeonameId : <b>{{ country.geonameId }}</b></li>
+            <li>Name : <b>{{ country.name }}</b></li>
+        </ul>
     </div>
 </template>
+
+<style scoped>
+ul {
+    padding: 0;
+    margin: 0.5em auto;
+    border: 1px solid transparent;
+    border-color: #f9f9f9;
+    border-radius: 0.2em;
+}
+
+li {
+    list-style-type: none;
+}
+
+li b {
+    color: #ffffff;
+}
+
+.loader {
+    margin-top: 0.5em;
+    width: 48px;
+    height: 48px;
+    border: 5px solid #FFF;
+    border-bottom-color: transparent;
+    border-radius: 50%;
+    display: inline-block;
+    box-sizing: border-box;
+    animation: rotation 1s linear infinite;
+}
+
+@keyframes rotation {
+    0% {
+        transform: rotate(0deg);
+    }
+
+    100% {
+        transform: rotate(360deg);
+    }
+}
+</style>
