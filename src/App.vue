fbc9d63e87971ea603875bc780fca359ee8c2cac
MathieuGTR <mathieu@gtrsuite.com>
2024-01-16 22:25:50 +0100

    chore(router): add sass support

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,27 @@
 <script setup>
 import CountryListFetch from './components/CountryListFetch.vue';
+import { useRouter, useRoute } from 'vue-router';
+import Sidebar from './components/Sidebar.vue';
+
 </script>
 
 <template>
-  <header>
-    <h1>Geonames <span class="logo">🌐</span></h1>
-  </header>
-  <CountryListFetch />
+  <!--<header>
+      <h1>Geonames <span class="logo">🌐</span></h1>
+    </header>
+    <nav class="menu">
+      <ul>
+        <li><a href="#">Export</a></li>
+        <li><a href="#">Translations</a></li>
+      </ul>
+    </nav>
+    <CountryListFetch />
+  -->
+  <!-- <RouterLink></RouterLink>
+  <RouterLink></RouterLink> -->
+  <Sidebar />
+  <RouterView />
+  <!-- <router-view /> -->
 </template>
 
 <style scoped>
@@ -23,5 +38,21 @@ import CountryListFetch from './components/CountryListFetch.vue';
 
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
+}
+
+ul {
+  padding: 0;
+  margin: 0.5em auto;
+  border: 1px solid transparent;
+  border-color: #f9f9f9;
+  border-radius: 0.2em;
+}
+
+li {
+  list-style-type: none;
+}
+
+li b {
+  color: #ffffff;
 }
 </style>
