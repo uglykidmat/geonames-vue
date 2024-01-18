7b608d1e9205f496505eb55eb1b319a52d0a3e3f
MathieuGTR <mathieu@gtrsuite.com>
2024-01-18 19:42:25 +0100

    feat(translations): view update + new component

---
+++ src/components/Translations.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { onUpdated, ref } from 'vue';
+
+const apiToken = import.meta.env.VITE_GEONAMES_TOKEN;
+const apiURL = import.meta.env.VITE_GEONAMES_URL;
+
+const loader = ref(false);
+
+</script>
+<template></template>
+<style scoped></style>
--- src/views/TranslationsView.vue
+++ src/views/TranslationsView.vue
@@ -1,6 +1,11 @@
+<script setup>
+import Translations from '../components/Translations.vue';
+</script>
+
 <template>
     <div class="translations">
         <h2>Translations</h2>
         <p>Geonames locales</p>
+        <Translations />
     </div>
 </template>
